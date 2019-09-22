4674534f72460e97955481add2836aed50cf40ea
ddgwilli <ddgwilli@edu.uwaterloo.ca>
2019-09-21 21:12:51 -0400

    Introduced an error into the code.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styles from './App.module.scss';
 
-const App: React.FC = () => (
-  <h1 className={styles.helloworld}>Hello CircleCI</h1>
-);
+const App: React.FC = () => {
+  const foo = 0;
+  return <h1 className={styles.helloworld}>Hello CircleCI</h1>;
+};
 
 export default App;
