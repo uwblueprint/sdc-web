05d551af1286d0b3ef7d71ee427f0478affc4be5
meganniu <megan.niu@gmail.com>
2019-11-21 21:59:14 -0500

    Add tests for UseAxios

--- src/utils/UseAxios.test.tsx
+++ src/utils/UseAxios.test.tsx
@@ -1,64 +1,85 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { useAxios } from './UseAxios';
+import { useAxios, ApiResponse } from './UseAxios';
 import { HttpMethods } from './HttpMethods';
 import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
 
-const successulResponseData = { message: 'Testing' };
-const successulResponse = {
-  data: successulResponseData,
+const httpHeaders = new Headers({
+  'Content-Type': 'json',
+  'Project-Name': 'sdc',
+});
+
+const getResponseData = { message: 'Testing successful response.' };
+const successfulGetResponse: ApiResponse = {
+  data: getResponseData,
   status: 200,
+  headers: httpHeaders,
 };
-const erroneousResponse = {
+
+const erroneousResponse: ApiResponse = {
   data: null,
   status: 404,
+  headers: httpHeaders,
 };
-const putResponse = {};
-const loadingResponse = {
-  response: null,
-  isLoading: true,
-  isSuccessful: false,
+
+const putRequestData = { message: 'Testing PUT request.' };
+const successfulPutResponse: ApiResponse = {
+  data: putRequestData,
+  status: 200,
+  headers: httpHeaders,
 };
-const url = 'https://randomuser.me/api';
+
+const postRequestData = { message: 'Testing POST request.' };
+const successfulPostResponse: ApiResponse = {
+  data: postRequestData,
+  status: 200,
+  headers: httpHeaders,
+};
+
+const successfulDeleteResponse: ApiResponse = {
+  data: null,
+  status: 200,
+  headers: httpHeaders,
+};
+
+const url = 'https://someapi';
+const urlPath = '/test';
 
 describe('useAxios', () => {
   it('returns isLoading while waiting for response', () => {
     const { result } = renderHook(() =>
-      useAxios(HttpMethods.GET, '/test', null)
+      useAxios(HttpMethods.GET, urlPath, null)
     );
 
     expect(result.current.isLoading).toEqual(true);
     expect(result.current.isSuccessful).toEqual(false);
   });
-  it('returns data if isSuccessful', async () => {
+
+  it('returns data if successful', async () => {
     const mock = new MockAdapter(axios);
     mock
-      .onAny(`${url}/test`)
-      .reply(successulResponse.status, successulResponseData);
+      .onGet(`${url}${urlPath}`)
+      .reply(successfulGetResponse.status, getResponseData, httpHeaders);
 
     const { result, waitForNextUpdate } = renderHook(() =>
-      useAxios(HttpMethods.GET, '/test', null)
+      useAxios(HttpMethods.GET, urlPath, null)
     );
-
-    expect(result.current.isLoading).toEqual(true);
-    expect(result.current.isSuccessful).toEqual(false);
 
     await waitForNextUpdate();
 
-    expect(result.current.response).toStrictEqual(successulResponse);
+    expect(result.current.response).toStrictEqual(successfulGetResponse);
     expect(result.current.isLoading).toEqual(false);
     expect(result.current.isSuccessful).toEqual(true);
   });
 
-  it('returns status text if unsuccessful', async () => {
+  it('indicates request is unsuccessful if response status is not 200', async () => {
     const mock = new MockAdapter(axios);
-    mock.onAny(`${url}/test`).reply(erroneousResponse.status, null);
+    mock
+      .onAny(`${url}${urlPath}`)
+      .reply(erroneousResponse.status, null, httpHeaders);
     const { result, waitForNextUpdate } = renderHook(() =>
-      useAxios(HttpMethods.GET, '/test', null)
+      useAxios(HttpMethods.GET, urlPath, null)
     );
-
-    expect(result.current.isLoading).toEqual(true);
-    expect(result.current.isSuccessful).toEqual(false);
 
     await waitForNextUpdate();
 
@@ -66,5 +87,52 @@ describe('useAxios', () => {
     expect(result.current.isSuccessful).toEqual(false);
     expect(result.current.response).toStrictEqual(erroneousResponse);
   });
-  it('handles put requests that send objects', () => {});
+
+  it('handles put requests that sends request data', async () => {
+    const mock = new MockAdapter(axios);
+    mock
+      .onAny(`${url}${urlPath}`)
+      .reply(successfulPutResponse.status, putRequestData, httpHeaders);
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useAxios(HttpMethods.PUT, urlPath, putRequestData)
+    );
+
+    await waitForNextUpdate();
+
+    expect(result.current.isLoading).toEqual(false);
+    expect(result.current.isSuccessful).toEqual(true);
+    expect(result.current.response).toStrictEqual(successfulPutResponse);
+  });
+
+  it('handles post requests that sends request data', async () => {
+    const mock = new MockAdapter(axios);
+    mock
+      .onAny(`${url}${urlPath}`)
+      .reply(successfulPostResponse.status, postRequestData, httpHeaders);
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useAxios(HttpMethods.POST, urlPath, postRequestData)
+    );
+
+    await waitForNextUpdate();
+
+    expect(result.current.isLoading).toEqual(false);
+    expect(result.current.isSuccessful).toEqual(true);
+    expect(result.current.response).toStrictEqual(successfulPostResponse);
+  });
+
+  it('handles delete requests', async () => {
+    const mock = new MockAdapter(axios);
+    mock
+      .onAny(`${url}${urlPath}`)
+      .reply(successfulDeleteResponse.status, null, httpHeaders);
+    const { result, waitForNextUpdate } = renderHook(() =>
+      useAxios(HttpMethods.DELETE, urlPath, null)
+    );
+
+    await waitForNextUpdate();
+
+    expect(result.current.isLoading).toEqual(false);
+    expect(result.current.isSuccessful).toEqual(true);
+    expect(result.current.response).toStrictEqual(successfulDeleteResponse);
+  });
 });
--- src/utils/UseAxios.ts
+++ src/utils/UseAxios.ts
@@ -1,36 +1,50 @@
 import { useState, useEffect } from 'react';
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
 import { HttpMethods } from './HttpMethods';
+
+export interface ApiResponse {
+  data: Object | null;
+  status: number;
+  headers: Headers;
+}
 
 export const useAxios = (
   httpMethod: HttpMethods,
   urlPath: string,
   requestData: any
 ) => {
-  const url = 'https://randomuser.me/api';
-  const [response, setResponse] = useState({ data: null, status: null });
+  const url = 'https://someapi';
+  const initialApiResponse: ApiResponse = {
+    data: null,
+    status: null,
+    headers: new Headers(),
+  };
+  const [response, setResponse] = useState(initialApiResponse);
   const [isLoading, setIsLoading] = useState(true);
   const [isSuccessful, setIsSuccessful] = useState(false);
-  const axiosConfig = {
+  const axiosConfig: AxiosRequestConfig = {
     method: httpMethod,
     url: url + urlPath,
     responseType: 'json',
     data: null,
   };
 
-  if (httpMethod.toUpperCase() === 'PUT' && requestData !== null) {
+  if (
+    (httpMethod === HttpMethods.PUT || httpMethod === HttpMethods.POST) &&
+    requestData !== null
+  ) {
     axiosConfig.data = requestData;
   }
 
   useEffect(() => {
     async function callApi() {
-      //
-
-      // await axios
-      //   .get(url + urlPath)
       await axios(axiosConfig)
-        .then((result: { data: Object; status: number }) => {
-          setResponse({ data: result.data, status: result.status });
+        .then((result: { data: Object; status: number; headers: Headers }) => {
+          setResponse({
+            data: result.data,
+            status: result.status,
+            headers: result.headers,
+          });
           setIsLoading(false);
           setIsSuccessful(true);
         })
@@ -39,12 +53,13 @@ export const useAxios = (
             response: {
               data: Object;
               status: number;
-              headers: Array<Array<string>>;
+              headers: Headers;
             };
           }) => {
             setResponse({
-              data: error.response.data ? error.response.data : null,
-              status: error.response.status,
+              data: error.response ? error.response.data : null,
+              status: error.response ? error.response.status : -1,
+              headers: error.response ? error.response.headers : new Headers(),
             });
             setIsLoading(false);
             setIsSuccessful(false);
@@ -52,7 +67,8 @@ export const useAxios = (
         );
     }
     callApi();
-  }, [axiosConfig, urlPath]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return { response, isLoading, isSuccessful };
 };
