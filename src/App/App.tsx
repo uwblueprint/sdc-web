9e8a4df558d8f357971c667bc5279357ac3f928e
AinleyP <ainleypena@gmail.com>
2020-01-16 20:48:46 -0500

    addedHook

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import { useRoutes, A } from 'hookrouter';
+import routes from "./router";
 import styles from './App.module.scss';
 
-const App: React.FC = () => <h1 className={styles.helloworld}>Hello SDC</h1>;
+import FlowChartNodeComponent from "./components/FlowChartNodeComponent";
+import FlowChartComponent from "./components/FlowChartComponent";
+
+//const App: React.FC = () => <h1 className={styles.helloworld}>Hello SDC</h1>;
+
+function App() {
+    const routeResult = useRoutes(routes);
+
+    return (
+        <div className="App">
+            <A href="/flowchart_node">Flowchart Node</A>
+            <A href="/flowchart">Flowchart</A>
+            {routeResult}
+        </div>
+    )
+}
 
 export default App;
