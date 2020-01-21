4405d62841fd99399a75988fccb4de632265530d
AinleyP <ainleypena@gmail.com>
2020-01-20 21:01:43 -0500

    fixeslint

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,27 +1,18 @@
 import React from 'react';
-import styles from './App.module.scss';
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-} from "react-router-dom";
-
+//import styles from './App.module.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App: React.FC = () => (
-    <Router>
-        <Switch>
-            <Route path="/home">
-                <p>Hello test</p>
-            </Route>
-            <Route path="/">
-                <p>First test</p>
-            </Route>
-        </Switch>
-    </Router>
-
+  <Router>
+    <Switch>
+      <Route path="/home">
+        <p>Hello test</p>
+      </Route>
+      <Route path="/">
+        <p>First test</p>
+      </Route>
+    </Switch>
+  </Router>
 );
-
-
 
 export default App;
