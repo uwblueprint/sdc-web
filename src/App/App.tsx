d0cd03a4f6bcc5e9cc748a79e9ee79eea5e5a177
James Lu <31876783+JamesZhaoLu@users.noreply.github.com>
2020-01-20 20:10:24 -0500

    Revert "Ainleyp branch (#7)"
    This reverts commit b9f63becc4c14ecdf1138034d0ec3771ec862200.

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
-import { useRoutes, A } from 'hookrouter';
-import routes from './router';
+import styles from './App.module.scss';
 
-//import FlowChartNodeComponent from "./components/FlowChartNodeComponent";
-//import FlowChartComponent from "./components/FlowChartComponent";
-
-function App() {
-    const routeResult = useRoutes(routes);
-
-    return (
-        <div className="App">
-            <A href="/flowchart_node">Flowchart Node</A>
-            <A href="/flowchart">Flowchart</A>
-            {routeResult}
-        </div>
-    )
-}
+const App: React.FC = () => <h1 className={styles.helloworld}>Hello SDC</h1>;
 
 export default App;
