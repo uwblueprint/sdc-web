1745620b828c3d877b8724f2d723bf502d47435e
Patrick Du <patrickduu@gmail.com>
2020-01-23 21:12:56 -0500

    Patrick add get flowchart endpoint (#10)
    * firstcommit
    
    * flowchart-api call
    
    * eslint-fix
    
    * remove debugger and fix eslint versioning

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 //import styles from './App.module.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Flowchart from './Components/Flowchart';
 
 const App: React.FC = () => (
   <Router>
     <Switch>
-      <Route path="/home">
-        <p>Hello test</p>
+      <Route path="/flowchart" component={Flowchart} />
+
+      <Route path="/flowchartnodes">
+        <p>Hi</p>
       </Route>
       <Route path="/">
         <p>First test</p>
