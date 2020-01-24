ee3d0fff0b0e5bfcaf5b2e173d0faff2e5e70e99
Aphexis <stephxca@gmail.com>
2020-01-23 21:39:00 -0500

    add FlowChartNodeComponent

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 //import styles from './App.module.scss';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import FlowChartNodeComponent from './components/FlowChartNodeComponent';
 
 const App: React.FC = () => (
   <Router>
     <Switch>
       <Route path="/home">
-        <p>Hello test</p>
+        <p>He</p>
       </Route>
-      <Route path="/">
+      {/* <Route path="/">
         <p>First test</p>
-      </Route>
+      </Route> */}
+      <Route path="/flowchartnode" component={FlowChartNodeComponent} />
     </Switch>
   </Router>
 );
