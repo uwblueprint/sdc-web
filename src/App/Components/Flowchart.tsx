184411ca376cdcfd260819764e73720842866f2b
Aphexis <stephxca@gmail.com>
2020-02-03 19:53:59 -0500

    view component connected to flowchart api

--- src/App/Components/Flowchart.tsx
+++ src/App/Components/Flowchart.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import { useGetFlowchartsApi } from '../../utils/FlowchartApi';
-
+// import { useEffect } from 'react';
 type MyProps = {
   message: string;
 };
 
 export default function FlowChart(props: MyProps) {
-  const testString = useGetFlowchartsApi();
+  const testString = useFlowchart;
+  // useEffect(() => {
+  //   testString = useFlowchart;
+  // });
   return <div>{JSON.stringify(testString)}</div>;
+}
+
+function useFlowchart() {
+  const testString = useGetFlowchartsApi();
+  // console.log(testString);
 }
--- src/App/Components/View.tsx
+++ src/App/Components/View.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { useGetFlowchartsApi } from '../../utils/FlowchartApi';
+// import { useEffect } from 'react';
 
 type MyProps = {
-  message: string;
+  // message: string;
 };
 
 export default function View(props: MyProps) {
-  const flowcharts = useGetFlowchartsApi().response;
-
-  const flowcharts_rendered = flowcharts.map((flowchart: JSON) => (
-    <li>{flowchart}</li>
+  const flowcharts = useGetFlowchartsApi();
+  if (flowcharts.isLoading) {
+    return <p>Loading</p>;
+  }
+  // console.log(flowcharts);
+  const flowcharts_rendered = flowcharts.response.map((flowchart: JSON) => (
+    <li>{JSON.stringify(flowcharts)}</li>
   ));
 
-  return flowcharts_rendered;
+  return <div>{flowcharts_rendered}</div>;
 }
