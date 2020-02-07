78f8bd0278dd24e8804dff46da90fa5ecefa167d
Peiyao Chen <p72chen@uwaterloo.ca>
2020-02-06 21:51:18 -0500

    Creating the two main views

---
+++ src/App/Components/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+  render() {
+    // render the components for the home page here
+    return <div>Home</div>;
+  }
+}
