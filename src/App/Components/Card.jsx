c33add2b6a22e151c1cd01528e69b80b079d34b2
Aphexis <stephxca@gmail.com>
2020-02-13 20:46:57 -0500

    made flowchart component

--- src/App/Components/Card.jsx
+++ src/App/Components/Card.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class Card extends React.Component {
   render() {
     return (
-      <div onClick={this.props.onClick}>flow chart {this.props.data.id}</div>
+      <div onClick={this.props.onClick}>
+        {this.props.title} {this.props.description}
+      </div>
     );
   }
 }
+
+Card.propTypes = {
+  id: PropTypes.number,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  onClick: PropTypes.func,
+};
--- src/App/Components/FlowChart.jsx
+++ src/App/Components/FlowChart.jsx
@@ -1,8 +1,40 @@
 import React from 'react';
+import Card from './Card.jsx';
+// add the flowchartnode api
+
 export default class FlowChart extends React.Component {
+  state = {
+    flowchartNodes: [
+      { id: 1, text: 'node text', header: 'this is a fake node' },
+    ],
+  };
+  componentDidMount() {
+    //api stuff here
+  }
   render() {
     // render the flow chart components here
     const { id } = this.props.match.params;
-    return <div>{id}</div>;
+    return (
+      <div>
+        {this.renderHeader()} {this.renderCards()}
+      </div>
+    );
+  }
+
+  renderHeader() {
+    const { flowchartNodes } = this.state;
+    return <div>{flowchartNodes[0].header}</div>;
+  }
+
+  renderCards() {
+    const { flowchartNodes } = this.state;
+    return flowchartNodes.map((flowchartNode) => (
+      <Card
+        id={flowchartNode.id}
+        title={flowchartNode.title}
+        description={flowchartNode.text}
+        onClick={() => this.setState({ flowcharNodes: [] })} //change this
+      />
+    ));
   }
 }
