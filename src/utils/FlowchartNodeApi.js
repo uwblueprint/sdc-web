bce25f1b941093e672b217c6b81c406c76d04255
Patrick Du <patrickduu@gmail.com>
2020-02-13 20:42:44 -0500

    Patrick flowchart node api changes (#16)
    * api
    
    * test changes

---
+++ src/utils/FlowchartNodeApi.js
@@ -0,0 +1,20 @@
+// import { useAxios, ApiHookResponse } from './UseAxios';
+import { HttpMethods } from './HttpMethods';
+import { req } from './Api.js';
+
+const baseUrl = `${process.env.REACT_APP_API_URL}/flowchart_node`;
+
+export const getFlowchartNode = (flowchartNodeId) =>
+  req(HttpMethods.GET, `${baseUrl}/${flowchartNodeId}`, null);
+
+export const updateFlowchartNode = (flowchartNodeId, request) =>
+  req(HttpMethods.PUT, `${baseUrl}/${flowchartNodeId}`, request);
+
+export const swapFlowchartNode = (request) =>
+  req(HttpMethods.PUT, `${baseUrl}/swap`, request);
+
+export const deleteFlowchartNode = (flowchartNodeId) =>
+  req(HttpMethods.DELETE, `${baseUrl}/${flowchartNodeId}`, null);
+
+export const createFlowchartNode = (request) =>
+  req(HttpMethods.POST, `${baseUrl}`, request);
