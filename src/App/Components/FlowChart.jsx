8da3cd084e5030524d8349de55ecb1f42add48f2
Faizaan Madhani <fzmadhani@gmail.com>
2020-02-27 21:25:11 -0500

    Flowchart styling

--- src/App/Components/FlowChart.jsx
+++ src/App/Components/FlowChart.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 import Card from './Card.jsx';
 import { getFlowchart } from '../../utils/FlowchartApi';
 import { getChildren } from '../../utils/FlowchartNodeApi';
+import Container from '@material-ui/core/Container';
+import { Box } from '@material-ui/core';
+import { spacing } from '@material-ui/system';
+
+const theme = {
+  spacing: 10,
+};
 
 export default class FlowChart extends React.Component {
   state = {
@@ -32,36 +39,59 @@ export default class FlowChart extends React.Component {
   render() {
     // render the flow chart components here
     // const { id } = this.props.match.params;
-    return <div>{this.renderCards()}</div>;
+    return (
+      <Container maxWidth="sm">
+        <div style={{ fontFamily: 'Arial' }}>
+          <div>{this.renderHeader()}</div>
+          <div>{this.renderCards()}</div>
+        </div>
+      </Container>
+    );
   }
 
   renderHeader() {
     const { flowchartNodes } = this.state;
-    return <div>{flowchartNodes[0].header}</div>;
+    console.log(flowchartNodes[0]);
+    return (
+      <Box
+        borderBottom={1}
+        pb={4.2}
+        pt={3.6}
+        pl={1.6}
+        pr={1.6}
+        borderColor={(196, 196, 196, 0.75)}
+      >
+        <div style={{ fontSize: '25px' }}>
+          {flowchartNodes.length !== 0 ? flowchartNodes[0].next_question : null}
+        </div>
+      </Box>
+    );
   }
 
   renderCards() {
     const { flowchartNodes } = this.state;
     console.log(flowchartNodes);
     return flowchartNodes.map((flowchartNode) => (
-      <Card
-        id={flowchartNode.id}
-        title={flowchartNode.title}
-        description={flowchartNode.text}
-        onClick={() =>
-          getChildren(flowchartNode.id)
-            .then(
-              (children) =>
-                this.setState({ flowchartNodes: children, isFirst: false }),
-              console.log('hi')
-            )
-            .catch(({ response }) => {
-              if (!response) {
-                console.log('Error fetching flowchart nodes');
-              }
-            })
-        }
-      />
+      <Box borderBottom={1} pt={2.4} pb={3.4} pl={2.4} pr={3.531}>
+        <Card
+          id={flowchartNode.id}
+          title={flowchartNode.title}
+          description={flowchartNode.text}
+          onClick={() =>
+            getChildren(flowchartNode.id)
+              .then(
+                (children) =>
+                  this.setState({ flowchartNodes: children, isFirst: false }),
+                console.log('hi')
+              )
+              .catch(({ response }) => {
+                if (!response) {
+                  console.log('Error fetching flowchart nodes');
+                }
+              })
+          }
+        />
+      </Box>
     ));
   }
 }
