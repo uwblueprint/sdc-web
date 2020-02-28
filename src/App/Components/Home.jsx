b479fd0ef3690edfcb41a70b449db0b016859c5b
AinleyP <ainleypena@gmail.com>
2020-02-27 21:16:25 -0500

    homecomponent

--- src/App/Components/Home.jsx
+++ src/App/Components/Home.jsx
@@ -1,6 +1,68 @@
 import React from 'react';
 import Card from './Card.jsx';
+import MenuBar from './MenuBar.jsx';
 import { getFlowcharts } from '../../utils/FlowchartApi';
+import styled from 'styled-components';
+
+const Banner = styled.div`
+  background: #09433C;
+  padding: 40px 24px;
+  // width: 375px;
+  //height: 254px;
+  left: 0px;
+  top: 100px;
+`;
+
+const BoldedWord = styled.span`
+  font-weight: bold;
+`;
+
+const HeaderTitle = styled.h1`
+  position: static;
+  width: 327px;
+  height: 87px;
+  left: 0px;
+  top: 0px;
+  color: #FFFFFF;
+  font-family: Arial;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 25px;
+  line-height: 29px;
+  letter-spacing: 1.5px;
+`;
+
+const CardQuestion = styled.h1`
+  padding: 42px 24px;
+  width: 375px;
+  height: 58px;
+  top: 399px;
+  left: 16px;
+  font-family: Arial;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 25px;
+  line-height: 29px;
+`;
+
+const HeaderDescription = styled.div`
+  width: 327px;
+  height: 69px;
+  left: 0px;
+  // top: 105px;
+  font-family: Arial;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 20px;
+  line-height: 23px;
+  color: #FFFFFF;
+  letter-spacing: 1.5px;
+`;
+
+const CardContainer = styled.div`
+
+  top:499px;
+`;
 
 export default class Home extends React.Component {
   state = {
@@ -18,6 +80,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
+        <MenuBar></MenuBar>
         {this.renderHeader()}
         {this.renderCards()}
       </div>
@@ -25,19 +88,42 @@ export default class Home extends React.Component {
   }
 
   renderHeader() {
-    return <div>Insert Title Text Here</div>;
+
+    return (
+      <Banner>
+        <HeaderTitle>
+          Welcome to the <BoldedWord>Social Development Center Waterloo Region.</BoldedWord>
+        </HeaderTitle>
+        <HeaderDescription>
+          Information and training resource for community level social services and programs.
+           </HeaderDescription>
+      </Banner>
+
+    );
   }
+
+
 
   renderCards() {
     const { flowcharts } = this.state;
     const { history } = this.props;
     return flowcharts.map((flowchart) => (
-      <Card
-        id={flowchart.id}
-        title={flowchart.title}
-        description={flowchart.description}
-        onClick={() => history.push(`/flowchart/${flowchart.id}`)}
-      />
+
+      <div>
+
+        <CardQuestion>
+          What type of information are you looking for?
+        </CardQuestion>
+        <CardContainer>
+          <Card
+            id={flowchart.id}
+            title={flowchart.title}
+            description={flowchart.description}
+            onClick={() => history.push(`/flowchart/${flowchart.id}`)}
+          />
+        </CardContainer>
+
+      </div>
     ));
   }
 }
